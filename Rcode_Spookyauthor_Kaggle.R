9c0a5778fb97346fb9bc8b66ef87a532872f784c
Aparna Adiraju <31944160+aparnaadiraju92@users.noreply.github.com>
2018-02-16 20:15:15 -0600

    Text Length code commit

--- Rcode_Spookyauthor_Kaggle.R
+++ Rcode_Spookyauthor_Kaggle.R
@@ -22,3 +22,28 @@ spooky.test <- read.csv("test.csv", stringsAsFactors = FALSE)
 View(spooky.train)
 View(spooky.test)
 
+# Check data to see if there are missing values.
+length(which(!complete.cases(spooky.train)))
+
+# Convert our class label into a factor.
+spooky.train$author <- as.factor(spooky.train$author)
+
+# The first step is to explore the data.
+# First, let's take a look at distribution of the class labels (i.e., EAP, HPL, MWS)
+prop.table(table(spooky.train$author))
+
+# Next up, the distribution of text lengths of the sentences 
+# by adding a new feature for the length of each message.
+spooky.train$TextLength <- nchar(spooky.train$text)
+summary(spooky.train$TextLength)
+View(spooky.train)
+
+# Visualize distribution with ggplot2, adding segmentation for authors
+library(ggplot2)
+
+ggplot(spooky.train, aes(x = TextLength, fill = author)) + theme_bw() +
+  geom_histogram(binwidth = 50) +
+  labs(y = "Text Count", x = "Length of Text",
+       title = "Distribution of Text Lengths with Class Labels")
+
+
