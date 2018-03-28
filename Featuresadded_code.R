112a1b0dc5e26c4323781d8ede0a2c96ac4759b0
Aparna Adiraju <31944160+aparnaadiraju92@users.noreply.github.com>
2018-03-27 23:28:17 -0500

    Prediction on test data

--- Featuresadded_code.R
+++ Featuresadded_code.R
@@ -1,8 +1,18 @@
+#Kaggle
+#Spooky Author Identification
 
+
+#--------Part-1--------#
+# 1. Installing required packages
+# 2. Loading packages
+# 3. Read data into a dataframe
+
+
+# 1. Installing required packages----------
 install.packages(c("plyr", "dplyr", "ldatuning", "tidyverse", "tidytext", "stringr", "tm", "topicmodels",
-                   "ggplot2","caret","readr","text2vec"))
+                   "ggplot2","caret","readr","text2vec", "xgboost", "e1071"))
 
-
+# 2. Loading packages------------
 library(plyr)
 library(dplyr)
 library(ldatuning)
@@ -15,110 +25,49 @@ library(ggplot2)
 library(caret)
 library("readr")
 library(text2vec)
+library(e1071)
+library(xgboost)
+library(randomForest)
+library(rpart)
 
-#read data
+# 3. Read train data provided into "train" variable creating a dataframe-----------
 train <- read.csv("train.csv", stringsAsFactors = FALSE)
 
+#reading in lexicon positive and negative words
+pos <- readLines("positive_words.txt")
+neg <- readLines("negative_words.txt")
 
-#topic modeling
+######################################################################
 
-my_source<-VectorSource(train$text)
-corpus<-Corpus(my_source)
-#corpus<-tm_map(corpus, function(x) iconv(x, to='UTF-8-MAC', sub='byte'))
-corpus<- tm_map(corpus, tolower)
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeWords, stopwords("english"))
-corpus<- tm_map(corpus, stemDocument)
-
-dtm <- DocumentTermMatrix(corpus, control=list(minDocFreq=2, minWordLength=2))
-
-#Remove empty cells and create a new corpus that aligns with the processed dtm
-rowTotals <- apply(dtm , 1, sum)
-empty.rows<-dtm[rowTotals == 0,]$dimnames[1][[1]]
-empty.rows<-as.numeric(empty.rows)
-corpus <- corpus[-empty.rows]
-
-#Create a dataframe of the new corpus
-corpus.df<-as.data.frame(corpus$content)
-
-#Create the dtm again with the new corpus
-dtm <- DocumentTermMatrix(corpus, control=list(minDocFreq=2, minWordLength=2))
-
-#Making sure that the original data set i.e., model.raw aligns with the new corpus. To do that we have to remove the same row numbers in empty.rows in model.raw
-x<-length(as.numeric(empty.rows))# calculate the number of empty.rows
+#--------Part-2--------#
+# 1. Writing required functions for analysis
+# 2. Data Exploration 
+#    - Topic Modeling
+#    - creating bigrams
+#    - creating trigrams
 
 
-#Write a loop that goes through the row numbers of model.raw and delete those rows that match with delete.rows
-empty.rows[1]
-for (i in 1:x){
-  model.raw<-model.raw[-empty.rows[i],]
-  i<-i+1
+# 1. Writing required functions for analysis
+#function to creare Document term matrix
+funcDTM <- function(train) {
+  my_source <- VectorSource(train$text)
+  corpus<-Corpus(my_source)
+  corpus<- tm_map(corpus, tolower)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  corpus<- tm_map(corpus, stemDocument)
+  
+  #Create the dtm again with the new corpus
+  dtm <- DocumentTermMatrix(corpus)
+  dtm <- removeSparseTerms(dtm, 0.997)
+  #Create a dataframe of the new corpus
+  spookyLabeledTerms<-as.data.frame(as.matrix(dtm))
+  
+  return(spookyLabeledTerms)
 }
 
-#Random check to see consistency between the new and old datasets
-corpus.df[743,]
-train[743,]$text
-
-#Run the LDA topic model
-lda<-LDA(dtm, k=9, control = list(seed=2343))
-
-#Using tidytext manifest topics
-topics<-tidy(lda, matrix="beta")
-topics
-
-spooky_topics <- topics
-
-spooky_top_terms <- spooky_topics %>%
-  group_by(topic) %>%
-  top_n(10, beta) %>%
-  ungroup() %>%
-  arrange(topic, -beta)
-
-spooky_top_terms %>%
-  mutate(term = reorder(term, beta)) %>%
-  ggplot(aes(term, beta, fill = factor(topic))) +
-  geom_col(show.legend = FALSE) +
-  facet_wrap(~ topic, scales = "free") +
-  coord_flip() + theme_bw()
-
-
-#bigrams
-spooky_bigrams <- train %>%
-  unnest_tokens(bigram, text, token = "ngrams", n = 2)
-
-spooky_bigrams
-
-#bigrams
-count_bigrams <- function(dataset) {
-  dataset %>%
-    unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
-    separate(bigram, c("word1", "word2"), sep = " ") %>%
-    filter(!word1 %in% stop_words$word,
-           !word2 %in% stop_words$word) %>%
-    count(word1, word2, sort = TRUE)
-}
-
-count_bigrams(train)
-
-#trigrams
-count_trigrams <- function(dataset) {
-  dataset %>%
-    unnest_tokens(trigram, text, token = "ngrams", n = 3) %>%
-    separate(trigram, c("word1", "word2", "word3"), sep = " ") %>%
-    filter(!word1 %in% stop_words$word,
-           !word2 %in% stop_words$word,
-           !word3 %in% stop_words$word)%>%
-    count(word1, word2, word3, sort = TRUE)
-}
-
-count_trigrams(train)
-
-#text length
-train$TextLength <- nchar(train$text)
-View(train)
-
-#comma features
-createFeature = function(ds)
+#comma features function
+createFeature <- function(ds)
 {
   ds = ds %>%
     mutate(commas = str_count(ds$text, ",")) %>%
@@ -127,20 +76,7 @@ createFeature = function(ds)
   return(ds)
 }
 
-train.Features <- createFeature(train)
-
-
-#sentiment
-pos <- readLines("positive_words.txt")
-neg <- readLines("negative_words.txt")
-
-spooky_sentiment <- as.data.frame(train.Features$text)
-colnames(spooky_sentiment)[1] <- "text"
-
-spooky_sentiment  <- sapply(spooky_sentiment ,function(row) iconv(row, "latin1", "ASCII", sub=""))
-
-
-#function to calculate sentiment score
+#sentiment analysis function
 score.sentiment <- function(sentences, pos.words, neg.words, .progress='none')
 {
   # Parameters
@@ -174,6 +110,91 @@ score.sentiment <- function(sentences, pos.words, neg.words, .progress='none')
   scores.df <- data.frame(text=sentences, score=scores)
   return(scores.df)
 }
+
+######################################################################
+
+######################################################################
+
+# 2. Data Exploration 
+spookyDTM <- funcDTM(train)
+spookyDTM <- spookyDTM[rowSums(abs(spookyDTM)) != 0,]
+
+#Run the LDA topic model
+lda<-LDA(spookyDTM, k=9, control = list(seed=2343))
+lda
+
+#Using tidytext to find the topics
+spooky_topics<-tidy(lda, matrix="beta")
+
+
+# Visualization 1 : Top 10 terms in each of the 9 topics-----------
+spooky_top_terms <- spooky_topics %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+
+spooky_top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip() + theme_bw()
+
+
+# Bigrams
+count_bigrams <- function(dataset) {
+  dataset %>%
+    unnest_tokens(bigram, text, token = "ngrams", n = 2) %>%
+    separate(bigram, c("word1", "word2"), sep = " ") %>%
+    filter(!word1 %in% stop_words$word,
+           !word2 %in% stop_words$word) %>%
+    count(word1, word2, sort = TRUE)
+}
+
+count_bigrams(train)
+
+
+#trigrams
+count_trigrams <- function(dataset) {
+  dataset %>%
+    unnest_tokens(trigram, text, token = "ngrams", n = 3) %>%
+    separate(trigram, c("word1", "word2", "word3"), sep = " ") %>%
+    filter(!word1 %in% stop_words$word,
+           !word2 %in% stop_words$word,
+           !word3 %in% stop_words$word)%>%
+    count(word1, word2, word3, sort = TRUE)
+}
+
+count_trigrams(train)
+
+######################################################################
+
+######################################################################
+
+#--------Part-3--------#
+# 1. Adding text length to train 
+# 2. Adding comma features to train
+# 3. Performing sentiment Analysis and adding sentiment scores to train
+# 4. Stratified Split
+
+
+# 1. Adding text length to train
+train$TextLength <- nchar(train$text)
+
+######################################################################
+
+# 2. Adding comma features to train
+train <- createFeature(train)
+
+######################################################################
+
+# 3. Sentiment analysis
+spooky_sentiment <- as.data.frame(train$text)
+colnames(spooky_sentiment)[1] <- "text"
+
+spooky_sentiment  <- sapply(spooky_sentiment ,function(row) iconv(row, "latin1", "ASCII", sub=""))
+
 #sentiment score
 sentiment_score <- score.sentiment(spooky_sentiment, pos, neg, .progress='text')
 summary(sentiment_score)
@@ -182,38 +203,214 @@ View(sentiment_score)
 #Convert sentiment scores from numeric to character to enable the gsub function 
 sentiment_score$sentiment <- as.character(sentiment_score$score)
 
-#After looking at the summary(scores_spNew$sentiment) decide on a threshold for the sentiment labels
+#After looking at the summary(sentiment_Score$sentiment) decide on a threshold for the sentiment labels
 sentiment_score$sentiment <- gsub("^0$", "Neutral", sentiment_score$sentiment)
 sentiment_score$sentiment <- gsub("^1$|^2$|^3$|^4$", "Positive", sentiment_score$sentiment)
 sentiment_score$sentiment <- gsub("^5$|^6$|^7$|^8$|^9$|^10$|^11$|^12$|^13$|^14$|^15$|^16$|^17$|^18$|^19$|^20$|^21$|^22$|^23$|^24$|^25$", "Very Positive", sentiment_score$sentiment)
 sentiment_score$sentiment <- gsub("^-1$|^-2$|^-3$|^-4$", "Negative", sentiment_score$sentiment)
 sentiment_score$sentiment <- gsub("^-5$|^-6$|^-7$|^-8$|^-9$|^-10$|^-11$|^-12$", "Very Negative", sentiment_score$sentiment)
 
+View(sentiment_score)
+
+#adding sentiment to train
+train$Sentimentscore <- sentiment_score[,2]
+train$SentimentLabel <- sentiment_score[,3]
+View(train)
+
+######################################################################
+
+#4. Stratified Split
+indexes <- createDataPartition(train$author, times = 1, p = 0.7, list = FALSE)
+split.train <- train[indexes,]
+split.test <- train[-indexes,]
+
+######################################################################
+
+######################################################################
+
+#--------Part-4--------#
+# 1. Preparing model for training
+# 2. Adding features to split.train and split.test sets
+# 3. Model building using XGBoost
+# 4. Predicting in split.test
+
+# 1. Preparing model for training
+spookyLabeledTerms<-funcDTM(split.train)
+spookyLabeledTerms.Test<-funcDTM(split.test)
+
+colnamesSame = intersect(colnames(spookyLabeledTerms),colnames(spookyLabeledTerms.Test))
+
+spookyLabeledTerms = spookyLabeledTerms[ , (colnames(spookyLabeledTerms) %in% colnamesSame)]
+spookyLabeledTerms.Test = spookyLabeledTerms.Test[ , (colnames(spookyLabeledTerms.Test) %in% colnamesSame)]
+
+######################################################################
+
+# 2. Adding features to split.train and split.test sets
+spookyLabeledTerms$Length <- split.train$TextLength
+spookyLabeledTerms.Test$Length <- split.test$TextLength
+
+spookyLabeledTerms$sentimentscore <- split.train$Sentimentscore
+spookyLabeledTerms.Test$sentimentscore <- split.test$Sentimentscore
+
+spookyLabeledTerms$Ncommas <- split.train$commas
+spookyLabeledTerms.Test$Ncommas <- split.test$commas
+
+spookyLabeledTerms$Nsemicolons <- split.train$semicolumns
+spookyLabeledTerms.Test$Nsemicolons <- split.test$semicolumns
+
+spookyLabeledTerms$Ncolons <- split.train$colons
+spookyLabeledTerms.Test$Ncolons <- split.test$colons
+
+spookyLabeledTerms$author = as.factor(split.train$author)
+levels(spookyLabeledTerms$author) = make.names(unique(spookyLabeledTerms$author))
+
+######################################################################
+
+# 3. Model building using XGBoost
+formula = author ~ .
+fitControl <- trainControl(method="none",classProbs=TRUE, summaryFunction=mnLogLoss)
+
+xgbGrid <- expand.grid(nrounds = 500,
+                       max_depth = 3,
+                       eta = .05,
+                       gamma = 0,
+                       colsample_bytree = .8,
+                       min_child_weight = 1,
+                       subsample = 1)
+
+
+set.seed(13)
+
+#Timing the code execution
+start.time <- Sys.time()
+
+AuthorXGB = train(formula, data = spookyLabeledTerms, 
+                  method = "xgbTree", trControl = fitControl, 
+                  tuneGrid = xgbGrid, na.action = na.pass, 
+                  metric="LogLoss", maximize=FALSE)
+
+total.time <- Sys.time() - start.time
+total.time
+
+# getting the importance of variables
+importance <- varImp(AuthorXGB)
+
+varImportance <- data.frame(Variables = row.names(importance[[1]]), 
+                            Importance = round(importance[[1]]$Overall,2))
+
+# Create a rank variable based on importance
+rankImportance <- varImportance %>%
+  mutate(Rank = paste0('#',dense_rank(desc(Importance)))) %>%
+  head(20)
+
+rankImportancefull = rankImportance
+
+ggplot(rankImportance, aes(x = reorder(Variables, Importance), 
+                           y = Importance)) +
+  geom_bar(stat='identity',colour="white", fill = "sky blue") +
+  geom_text(aes(x = Variables, y = 1, label = Rank),
+            hjust=0, vjust=.5, size = 4, colour = 'black',
+            fontface = 'bold') +
+  labs(x = 'Top Variables', title = 'XG Boost Model : Relative Variable Importance', 
+       y = 'Importance Score') +
+  coord_flip() + 
+  theme_bw()
+
+
+######################################################################
+
+# 4. Predicting in split.test
+# predicting the author value from the spookyLabeledTerms.Test data
+yXb_predict <- predict(AuthorXGB, newdata = spookyLabeledTerms.Test)
+yXb_predict_prob <- predict(AuthorXGB, newdata = spookyLabeledTerms.Test, type = "prob")
+
+
+# creation confusion matrix 
+cmXGB = table(split.test[,3], yXb_predict)
+cmXGB
+
+
+######################################################################
+
+######################################################################
+
+#--------Part-5--------#
+# Performing prediction on test data set provided by Kaggle
+# 1. Read the dataset to a variable 'test'
+# 2. Adding text length to test
+# 3. Adding comma features to test
+# 4. Adding Sentiment Analysis
+
+
+# 1. Reading dataset to variable 'test'
+test <- read.csv("test.csv", stringsAsFactors = FALSE)
+test <- read_csv("test.csv")
+test$author <- NA
+
+# 2. Adding text length to test
+test$TextLength <- nchar(test$text)
+
+# 3. Adding comma features to test
+test <- createFeature(test)
+
+# 4. Adding Sentiment analysis
+spooky_sentiment.test <- as.data.frame(test$text)
+colnames(spooky_sentiment.test)[1] <- "text"
+
+spooky_sentiment.test  <- sapply(spooky_sentiment.test ,function(row) iconv(row, "latin1", "ASCII", sub=""))
+
+#sentiment score
+sentiment_score <- score.sentiment(spooky_sentiment.test, pos, neg, .progress='text')
+summary(sentiment_score)
+View(sentiment_score)
+
+#Convert sentiment scores from numeric to character to enable the gsub function 
+sentiment_score$sentiment <- as.character(sentiment_score$score)
+
+#After looking at the summary(sentiment_Score$sentiment) decide on a threshold for the sentiment labels
+sentiment_score$sentiment <- gsub("^0$", "Neutral", sentiment_score$sentiment)
+sentiment_score$sentiment <- gsub("^1$|^2$|^3$|^4$", "Positive", sentiment_score$sentiment)
+sentiment_score$sentiment <- gsub("^5$|^6$|^7$|^8$|^9$|^10$|^11$|^12$|^13$|^14$|^15$|^16$|^17$|^18$|^19$|^20$|^21$|^22$|^23$|^24$|^25$", "Very Positive", sentiment_score$sentiment)
+sentiment_score$sentiment <- gsub("^-1$|^-2$|^-3$|^-4$", "Negative", sentiment_score$sentiment)
+sentiment_score$sentiment <- gsub("^-5$|^-6$|^-7$|^-8$|^-9$|^-10$|^-11$|^-12$", "Very Negative", sentiment_score$sentiment)
 
 View(sentiment_score)
 
-
-#adding sentiment to train.features
-train.Features$Sentimentscore <- sentiment_score[,2]
-train.Features$SentimentLabel <- sentiment_score[,3]
-View(train.Features)
-
-#------------ Stratified split of train data into split.train(70%), split.test(30%)
-
-library(caret)
-
-# Using caret to create a 70%/30% stratified  split. 
-# Random seed set for reproducibility.
-
-set.seed(3284)
-indexes <- createDataPartition(train.Features$author, times = 1, p = 0.7, list = FALSE)
-split.train <- train.Features[indexes,]
-split.test <- train.Features[-indexes,]
-
-prop.table(table(split.train$author))
-prop.table(table(split.test$author))
+#adding sentiment to train
+test$Sentimentscore <- sentiment_score[,2]
+test$SentimentLabel <- sentiment_score[,3]
+View(test)
 
 
-View(split.train)
-View(split.test)
+######################################################################
+
+# 5. Preparing model to apply model
+
+spookyLabeled <-funcDTM(test)
+
+colnamesSame = intersect(colnames(spookyLabeledTerms),colnames(spookyLabeled))
+ 
+spookyLabeled = spookyLabeled[ , (colnames(spookyLabeled) %in% colnamesSame)]
+
+spookyLabeled$Length <- test$TextLength
+
+spookyLabeled$sentimentscore <- test$Sentimentscore
+
+spookyLabeled$Ncommas <- test$commas
+
+spookyLabeled$Nsemicolons <- test$semicolumns
+
+spookyLabeled$Ncolons <- test$colons
+
+# predicting the author value from the Test data
+yXb_test_predict <- predict(AuthorXGB, spookyLabeled)
+yXb_test_predict_prob <- predict(AuthorXGB, spookyLabeled, type = 'prob')
+
+# Save the solution to a dataframe
+solution <- data.frame('id' = test$id, yXb_test_predict)
+solution <- data.frame('id' = test$id, yXb_test_predict_prob)
+
+
+
+
 
